717e3f8027a21203b71553ae8520104045cb5a4f
Roshna Magar <roshna.magar@stu.jefferson.kyschools.us>
2018-03-12 08:25:43 -0400

    first commit

---
+++ roshna_magar-assign01.py
@@ -0,0 +1,14 @@
+title = input("what is the title")
+media_type =input('what is the media type")
+game = input("what is the game")
+genre= input("what is the genre")
+description = input("descrip of media_type")
+rating = input("what is the rating")
+year = input("which year it's realease")
+rating = float input("what is rating")                    
+var = input("what is input")
+
+new_list (title,media_type,game,genre,description,year,rating)
+print = ("title,year,description,media_type,rating")
+                  
+                  
